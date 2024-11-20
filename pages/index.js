c3eaae9903a72bcc68c70520ab23d4186f9f6931
Diego Alonso <64498328+alonsopublicidade@users.noreply.github.com>
2024-11-20 16:11:18 +0000

    adicionar dominio

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 function Home() {
-  return <h1>Alterei</h1>;
-}
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
+  return <h1>Mercado da Rosa</h1>;
 }
 
 export default Home;
